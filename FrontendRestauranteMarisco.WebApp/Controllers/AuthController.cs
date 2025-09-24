accc68e14e9de1a4cae45412132284452b0ade63
kenisdelgado <kenyasuncin@gmail.com>
2025-09-24 10:49:57 -0600

    Se agregado los controller y se agrego el Login con la carpeta

---
+++ FrontendRestauranteMarisco.WebApp/Controllers/AuthController.cs
@@ -0,0 +1,73 @@
+﻿using FrontendRestauranteMarisco.WebApp.DTOs.UsuarioDTOs;
+using FrontendRestauranteMarisco.WebApp.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontendRestauranteMarisco.WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        // GET: Mostrar Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        // POST: Login
+        [HttpPost]
+        public async Task<IActionResult> Login(UsuarioLoginDTO dto)
+        {
+            var result = await _authService.LoginAsync(dto);
+            if (result == null)
+            {
+                ViewBag.Error = "Credenciales inválidas";
+                return View();
+            }
+
+            // Crear y firmar los claims usando el helper
+            var principal = ClaimsHelper.CrearClaimsPrincipal(result);
+
+            await HttpContext.SignInAsync("AuthCookie", principal);
+
+            return RedirectToAction("Index", "Home");
+        }
+        // POST: Registro
+        [HttpPost]
+        public async Task<IActionResult> Registrar(UsuarioRegistroDTO dto)
+        {
+            var result = await _authService.RegistrarAsync(dto);
+            if (result == null || result.Id <= 0)
+            {
+                ViewBag.Error = "Error al registrar";
+                return View();
+            }
+
+            // Crear y firmar los claims usando el helper
+            var principal = ClaimsHelper.CrearClaimsPrincipal(result);
+
+            await HttpContext.SignInAsync("AuthCookie", principal);
+
+            return RedirectToAction("Index", "Home");
+        }
+        // Logout
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync("AuthCookie");
+            return RedirectToAction("Login");
+        }
+
+        // GET: Mostrar Registro
+        [HttpGet]
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ FrontendRestauranteMarisco.WebApp/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using FrontendRestauranteMarisco.WebApp.DTOs.UsuarioDTOs;
+using FrontendRestauranteMarisco.WebApp.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+namespace FrontendRestauranteMarisco.WebApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly ApiService _apiService;
+
+        public UsuarioController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        // Listar usuarios
+        public async Task<IActionResult> Index()
+        {
+            var token = AuthHelper.ObtenerToken(User); // Obtener token desde Claims
+            var usuarios = await _apiService.GetAllAsync<UsuarioDTO>("User/usuarios", token);
+            return View(usuarios);
+        }
+    }
+}
